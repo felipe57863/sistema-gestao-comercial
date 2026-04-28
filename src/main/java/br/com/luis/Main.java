e2e19daf977f7c91674ebb20a6d75f217929f9b3
Luís Felipe Bueno <felipe57863@outlook.com>
2026-04-28 15:46:23 -0300

    build: injeta dependencias do javafx, sqlite e jbcrypt

--- src/main/java/br/com/luis/Main.java
+++ src/main/java/br/com/luis/Main.java
@@ -1,17 +1,26 @@
 package br.com.luis;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import br.com.luis.util.ConnectionFactory;
+import br.com.luis.util.DatabaseInitializer;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Cria/verifica tabelas
+        DatabaseInitializer.criarTabelas();
+
+        // Teste de conexão
+        try (Connection conn = ConnectionFactory.getConnection()) {
+
+            if (conn != null) {
+                System.out.println("Conexão com SQLite realizada com sucesso!");
+            }
+
+        } catch (Exception e) {
+            System.err.println("Erro ao testar conexão: " + e.getMessage());
         }
     }
 }
